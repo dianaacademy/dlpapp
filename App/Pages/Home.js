e4aee7f7dfebe4e8eb690a17da6d117ce8b6ceab
rrs301 <innocruts@gmail.com>
2023-05-05 11:41:48 -0400

    Slider Code

--- App/Pages/Home.js
+++ App/Pages/Home.js
@@ -1,17 +1,23 @@
 import { View, Text } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Button } from 'react-native'
 import Services from '../Shared/Services'
 import { AuthContext } from '../Context/AuthContext'
 import WelcomeHeader from '../Components/WelcomeHeader'
 import SearchBar from '../Components/SearchBar'
+import GlobalApi from '../Shared/GlobalApi'
+import Slider from '../Components/Slider'
+
+
 
 export default function Home() {
     const {userData,setUserData}=useContext(AuthContext)
+   
   return (
     <View style={{padding:20}}>
         <WelcomeHeader/>
         <SearchBar/>
+        <Slider/>
         
     </View>
   )
